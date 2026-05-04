600fa3a8bcefd14c5c514b170015ce0c3db13700
Abin02-web <liyaavinod@gmail.com>
2026-05-04 09:21:32 +0400

    Add files via upload

---
+++ L2homework.py
@@ -0,0 +1,12 @@
+Eben="2-10-2014"
+Zahran="26-6-2015"
+Dhruv="31-1-2013"
+Dhruhin="20-4-2016"
+Shaun="1-12-2015"
+print("Shauns birthday is on", Shaun)
+print("\nZahrans birthday is on", Zahran)
+print("\nDhruvs birthday is on", Dhruv)
+print("\nDhruhins birthday is on", Dhruhin)
+print("\nEbens birthday is on", Eben)
+input("Who is the oldest here?: ")
+print("Dhruv is the right answer")
